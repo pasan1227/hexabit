d9cd1d430ebd3b218ab7aa237503de67df9318ac
Pasan <pasanratnayake@gmail.com>
2023-01-15 00:34:40 +0530

    font sizes changed

--- src/pages/Earn/index.jsx
+++ src/pages/Earn/index.jsx
@@ -6,55 +6,55 @@ import './earn.css'
 const Earn = () => {
   return (
     <section className='earn-container' id='earn'>
-      <div className="left-side">
-        <div className="left-top">
+      <div className='left-side'>
+        <div className='left-top'>
           <h1>START EARNING</h1>
           <p>
             A remote workplace and a growing <br />
             company is waiting for you.
           </p>
-          <ButtonTwo text='APPLY NOW' icon='less-than'/>
+          <ButtonTwo text='APPLY NOW' icon='less-than' />
         </div>
-        <div className="left-bottom">
-          <img src={globe} alt="" />
+        <div className='left-bottom'>
+          <img src={globe} alt='' />
         </div>
       </div>
-      <div className="right-side">
-        <div className="right-top">
-          <p className='heading'>ARE YOU ACTIVE <br />
+      <div className='right-side'>
+        <div className='right-top'>
+          <p className='heading'>
+            ARE YOU ACTIVE <br />
             ON SOCIAL MEDIA?
           </p>
-          <p>
+          <p className='description'>
             Start earning by joining our affiliate <br />
             program. You'll offer products and <br />
             services made by hexabit.
           </p>
         </div>
-        <div className="right-bottom">
-          <div className="step-container">
+        <div className='right-bottom'>
+          <div className='step-container'>
             <div className='step'>
-              <p>1</p>
+              <p className='number'>1</p>
               <p>APPLY</p>
+            </div>
+            <div className='step'>
+              <p className='number'>2</p>
+              <p>COLLAB</p>
               <p>icon</p>
             </div>
             <div className='step'>
-              <p>1</p>
-              <p>APPLY</p>
-              <p>icon</p>
-            </div>
-            <div className='step'>
-              <p>1</p>
-              <p>APPLY</p>
+              <p className='number'>3</p>
+              <p>EARN</p>
               <p>icon</p>
             </div>
           </div>
           <div className='apply-btn'>
-          <ButtonTwo text='APPLY NOW' icon='less-than'/>
+            <ButtonTwo text='APPLY NOW' icon='less-than' />
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default Earn
