378343ac65a195e43d93f1204f4e019248c51bf6
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-14 12:50:08 +0530

    commit

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <div className='footer-container'>
       <div className="footer-top">
-        <div className="footer-top-left"></div>
+        <div className="footer-top-left">
+          <h1>hexabit</h1>
+        </div>
         <div className="footer-top-right"></div>
       </div>
       <div className="footer-bottom"></div>
