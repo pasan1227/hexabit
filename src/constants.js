5052147ee42135b26f301ab8d4909a8dfceccc3e
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-14 16:01:29 +0530

    Modularize footer with constants

---
+++ src/constants.js
@@ -0,0 +1,47 @@
+export const communityLinks = [
+  {
+    id: 1,
+    icon: 'tiktok',
+    to: 'https://www.tiktok.com/'
+  },
+  {
+    id: 2,
+    icon: 'linkedin',
+    to: 'https://www.linkedin.com/'
+  },
+  {
+    id: 3,
+    icon: 'twitter',
+    to: 'https://www.twitter.com/'
+  },
+  {
+    id: 4,
+    icon: 'instagram',
+    to: 'https://www.instagram.com/'
+  },
+]
+
+export const founderLinks = [
+  {
+    id: 1,
+    icon: 'spotify',
+    to: 'https://www.spotify.com/'
+  },
+  {
+    id: 2,
+    icon: 'youtube',
+    to: 'https://www.youtube.com/'
+  },
+  {
+    id: 3,
+    icon: 'linkedin',
+    to: 'https://www.linkedin.com/'
+  },
+  {
+    id: 4,
+    icon: 'twitter',
+    to: 'https://www.twitter.com/'
+  },
+]
+
+export const footerHeadings = ['STRATEGY', 'MEDIA', 'CHAIN', 'XPLOR']
--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { communityLinks, footerHeadings, founderLinks } from '../../constants'
 import Link from '../../components/Link'
 import './footer.css'
 
 const Footer = () => {
+  
+  
   return (
     <div className='footer-container'>
       <div className="footer-top">
@@ -16,53 +19,31 @@ const Footer = () => {
           <div className="footer-community">
             <h2>MEET OUR COMMUNITIES</h2>
             <div className="links">
-
-              <Link to='' icon='tiktok' />
-              <Link to='' icon='linkedin' />
-              <Link to='' icon='twitter' />
-              <Link to='' icon='instagram' />
+              {communityLinks.map((link) => (
+                <Link to={link.to} icon={link.icon} key={link.id} />
+              ))}
             </div>
           </div>
           <div className='footer-founder'>
             <h2>MEET OUR FOUNDER</h2>
             <div className="links">
-              <Link to='' icon='spotify' />
-              <Link to='' icon='youtube' />
-              <Link to='' icon='linkedin' />
-              <Link to='' icon='twitter' />
+              {founderLinks.map((link) => (
+                <Link to={link.to} icon={link.icon} key={link.id} />
+              ))}
             </div>
           </div>
         </div>
         <div className="footer-top-right">
           <div className="footer-right-headings">
-            <div className="heading">
-              <h1 className='heading-h1'>STRATEGY</h1>
-              <div className='footer-sub-heading'>
-                <span className='o'>O</span>
-                <span>NE</span>
+            {footerHeadings.map((heading, index) => (
+              <div className="heading" key={index}>
+                <h1 className='heading-h1'>{heading}</h1>
+                <div className='footer-sub-heading'>
+                  <span className='o'>O</span>
+                  <span>NE</span>
+                </div>
               </div>
-            </div>
-            <div className="heading">
-              <h1 className='heading-h1'>MEDIA</h1>
-              <div className='footer-sub-heading'>
-                <span className='o'>O</span>
-                <span>NE</span>
-              </div>
-            </div>
-            <div className="heading">
-              <h1 className='heading-h1'>CHAIN</h1>
-              <div className='footer-sub-heading'>
-                <span className='o'>O</span>
-                <span>NE</span>
-              </div>
-            </div>
-            <div className="heading">
-              <h1 className='heading-h1'>XPLOR</h1>
-              <div className='footer-sub-heading'>
-                <span className='o'>O</span>
-                <span>NE</span>
-              </div>
-            </div>
+            ))}
             <div className="heading">
               <h1 className='heading-h1'>CHARLIE Z 360</h1>
             </div>
