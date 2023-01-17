f45ef7188b90ed4c965449e172074c3236cda938
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-17 22:36:17 +0530

    bug fix

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import './button.css';
 const Button = ({ text, icon, onClick }) => {
     return (
         <div className="custom-button" onClick={onClick}>
-            <span className="button">{text}</span>
+            <span>{text}</span>
             <div className='icon'>
             <i className={`fa fa-${icon} button-icon`} />
             </div>
