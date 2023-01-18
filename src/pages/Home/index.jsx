377c4e82dd9b8689b72f5b8d6ae06dd4d7850bf9
Prathibha Ratnayake <prsthibha@gmail.com>
2023-01-18 13:46:57 +0530

    Add mouse and square

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
-import ParticleBackground from '../../components/Particle';
+import square from '../../assets/square.png';
 import './home.css';
+import ParticleBackground from '../../components/Particle'
 
 const Home = () => {
   return (
     <section className='home-container' id='home'>
       <div className='particle-container'>
         {/* <ParticleBackground /> */}
-      {/* <canvas
+        {/* <canvas
         style={{ height:'100%', width: '100%', pointerEvents: 'initial' }}
         width='1600'
         id='tsparticles'>
         <Particle />
       </canvas> */}
-      <img src={logo} alt='' />
-      <p>WHERE DIGITAL ASSETS MEET BLOCKCHAIN TECH</p>
+      <img src={square} alt="" className="square-bg" id="head-square" style={{opacity: 1, visibility: 'inherit', transform: 'translate(0px, 0px)'}}/>
+        <img src={logo} alt='' />
+        <p>WHERE DIGITAL ASSETS MEET BLOCKCHAIN TECH</p>
+        <div class="field">
+          <div class="mouse"></div>
+        </div>
       </div>
     </section>
   );
